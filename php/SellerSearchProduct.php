064c81346d13826cfc3a1a0225bf57efdfd3ceb4
S0ykot <soykot.ch@gmail.com>
2019-12-18 21:13:29 +0600

    SQL data change & some code

--- php/SellerSearchProduct.php
+++ php/SellerSearchProduct.php
@@ -28,6 +28,11 @@ if ($_SESSION['name'] AND $_SESSION['type'] AND $_COOKIE['timeout']) {
 		<th>Quantity</th>
 		<th>Buying price</th>
 		<th>Selling price</th>
+		<th>Incoming Date</th>
+		<th>Description</th>
+		<th>Category</th>
+		<th>Sub-Category</th>
+		<th>Image</th>
 		<th>Activity</th>
 		<th>Action</th>
 	</tr>
@@ -49,6 +54,11 @@ if ($_SESSION['name'] AND $_SESSION['type'] AND $_COOKIE['timeout']) {
 				<td align='center'>".$row['quantity']."</td>
 				<td align='center'>".$row['buying_price']."</td>
 				<td align='center'>".$row['selling_price']."</td>
+				<td align='center'>".$row['incoming_date']."</td>
+				<td align='center'>".$row['description']."</td>
+				<td align='center'>".$row['cat_name']."</td>
+				<td align='center'>".$row['subcat_name']."</td>
+				<td align='center'><img src='../upload/".$row['image']."' height='100' width='100'></td>
 				<td align='center'>".$act."</td>
 				<td align='center'><button id='edit'>Edit</button> &nbsp <button id='del'>Delete</button></td>
 				</tr>
