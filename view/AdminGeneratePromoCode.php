893a2294069ffc3226a1896dea4b582b74251a96
Khandakar Anim Hassan Adnan <khandakar.adnan21@gmail.com>
2019-12-22 01:00:43 +0600

    1.0.6- Promo code feature added

--- view/AdminGeneratePromoCode.php
+++ view/AdminGeneratePromoCode.php
@@ -1,4 +1,5 @@
 <?php 
+	require_once('../db/AdminPromoFunction.php');
 	session_start();
 
 	if (isset($_SESSION['username'])) {	
@@ -61,7 +62,10 @@
 		  	</div>
 		</div>
 	</div>
-	<form method="POST" action="">
+	<?php 
+		$data = getPromoLastId();
+	 ?>
+	<form method="POST" action="../php/AdminGeneratePromoCodeCheck.php">
 		<table align="center" bgcolor="CornflowerBlue" cellspacing="30px">
 			<tr>
 				<td colspan="4">
@@ -82,13 +86,14 @@
 					Promo Code ID:
 				</td>
 				<td>
-					<input type="text" name="prid" >
+					<input type="text" name="prid" value="<?php echo $data['did']+1; ?>" disabled>
 				</td>
 				<td>
 					Promo Code:
 				</td>
 				<td>
-					<input type="text" name="prname" placeholder="Enter Promo Code">
+					<input type="text" name="prname" placeholder="Enter Promo Code" id="pcode" onkeyup="validatePromoCode()">
+					<div id="erpcode" style="color: red;font-weight: bold;"></div>
 				</td>
 			</tr>
 			<tr>
@@ -96,13 +101,15 @@
 					Amount of Discount:
 				</td>
 				<td>
-					<input type="text" name="dis" placeholder="Enter Discount Amount in %">
+					<input type="text" name="dis" placeholder="Enter Discount Amount in %" id="pdis" onkeyup="validateDiscount()">
+					<div id="erpdis" style="color: red;font-weight: bold;"></div>
 				</td>
 				<td>
 					Validity:
 				</td>
 				<td>
-					<input type="date" name="prval">
+					<input type="date" name="prval" id="prdate" min="2001-01-01" max="2400-12-31" onclick="validatePromoDate()" onkeyup="validatePromoDate()"> 
+					<div id="erprdate" style="color: red;font-weight: bold;"></div>
 				</td>
 			</tr>
 			<tr>
@@ -110,11 +117,19 @@
 					Status:
 				</td>
 				<td>
-					<select name="prstat">
+					<select name="prstat" id="prstat" onchange="validatePromoStatus()">
 						<option value="">Select Status</option>
 						<option value="Enable">Enable</option>
 						<option value="Disable">Disable</option>
 					</select>
+					<div id="erprstat" style="color: red;font-weight: bold;"></div>
+				</td>
+				<td>
+					Customer ID:
+				</td>
+				<td>
+					<input type="text" name="cid" placeholder="Enter Customer ID" id="cid" onkeyup="validateCID()">
+					<div id="ercid" style="color: red;font-weight: bold;"></div>
 				</td>
 			</tr>
 			<tr>
